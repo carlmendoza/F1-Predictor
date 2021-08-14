8715c34b75d2a6f6e5d614145251c9e52a814f37
Carl Mendoza <carlm@kainos.com>
2021-08-14 16:24:23 +0100

    form done

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,84 +1,95 @@
 import { useState } from 'react'
 import Drivers from './Drivers'
+import { useForm } from 'react-hook-form'
 
 function Body() {
-    const [winner, setWinner] = useState('');
-    const [second, setSecond] = useState('');
-    const [third, setThird] = useState('');
+    const { register, handleSubmit, errors } = useForm();
+    const [podium, setPodium] = useState([]);
 
-    const onChange = (e) => {
-        setWinner(e.target.value);
-        console.log(winner);
+    const onSubmit = data => {
+        console.log(data);
+    }
+    
+    const addToPodium = (pos, name) => {
+        let newPodium = podium.slice();
+
+        if (newPodium.some(i => i.pos === pos)) {
+            newPodium.splice(newPodium.findIndex(d => d.pos === pos), 1);
+        }
+
+        newPodium.push({pos, name});
+        setPodium(newPodium);
     };
 
     const foo = () => { return false };
 
     return (
    
-    <section className="section" id="s-fullheight-100vh">      <p>Winner: {winner} Second: {second} Third: {third}</p>
-        <div className="field is-horizontal">
-            <div className="field-label is-normal">
-                <label className="label">Username</label>
-            </div>
-            <div className="field-body">
-                <div className="field">
-                    <div className="control">
-                        <input className="input is-normal" style={{width: "40%"}} type="text" placeholder="Enter your user name"/>
-                    </div>
+    <section className="section" id="s-fullheight-100vh">
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <div className="field is-horizontal">
+                <div className="field-label is-normal">
+                    <label className="label">Username</label>
                 </div>
-            </div>
-        </div>
-
-        <div className="field is-horizontal">
-            <div className="field-label is-normal">
-                <label className="label">Race winner</label>
-            </div>
-            <div className="field-body">
-                <div className="field">
-                    <div className="control">
-                        <div className="select" onChange={ (e) => setWinner(e.target.value) }>
-                            <select>
-                                <Drivers selectedDriver={ winner }/>
-                            </select>
+                <div className="field-body">
+                    <div className="field">
+                        <div className="control">
+                            <input {...register("user")} className="input is-normal" style={{width: "40%"}} type="text" placeholder="Enter your user name"/>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
 
-        {/* <div className="field is-horizontal">
-            <div className="field-label is-normal">
-                <label className="label">2nd place</label>
-            </div>
-            <div className="field-body">
-                <div className="field">
-                    <div className="control">
-                        <div className="select" onChange={ (e) => setSecond(e.target.value) }>
-                            <select>
-                                <Drivers/>
-                            </select>
+            <div className="field is-horizontal">
+                <div className="field-label is-normal">
+                    <label className="label">Race winner</label>
+                </div>
+                <div className="field-body">
+                    <div className="field">
+                        <div className="control">
+                            <div className="select" onChange={ (e) => addToPodium(1, e.target.value) }>
+                                <select {...register("first")}>
+                                    <Drivers podium={ podium }/>
+                                </select>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
 
-        <div className="field is-horizontal">
-            <div className="field-label is-normal">
-                <label className="label">3rd place</label>
-            </div>
-            <div className="field-body">
-                <div className="field">
-                    <div className="control">
-                        <div className="select" onChange={ (e) => setThird(e.target.value) }>
-                            <select>
-                                <Drivers/>
-                            </select>
+            <div className="field is-horizontal">
+                <div className="field-label is-normal">
+                    <label className="label">2nd place</label>
+                </div>
+                <div className="field-body">
+                    <div className="field">
+                        <div className="control">
+                            <div className="select" onChange={ (e) => addToPodium(2, e.target.value) }>
+                                <select {...register("second")}>
+                                    <Drivers podium={ podium }/>
+                                </select>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div> */}
+
+            <div className="field is-horizontal">
+                <div className="field-label is-normal">
+                    <label className="label">3rd place</label>
+                </div>
+                <div className="field-body">
+                    <div className="field">
+                        <div className="control">
+                            <div className="select" onChange={ (e) => addToPodium(3, e.target.value) }>
+                                <select {...register("third")}>
+                                    <Drivers podium={ podium }/>
+                                </select>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         
         {/* <div className="field is-horizontal">
             <div className="field-label is-normal">
@@ -97,18 +108,19 @@ function Body() {
             </div>
         </div> */}
 
-        <div className="field is-horizontal">
-            <div className="field-label"></div>
-            <div className="field-body">
-                <div className="field">
-                    <div className="control">
-                        <button className="button is-info">
-                            Submit predictions
-                        </button>
+            <div className="field is-horizontal">
+                <div className="field-label"></div>
+                <div className="field-body">
+                    <div className="field">
+                        <div className="control">
+                            <button className="button is-info">
+                                Submit predictions
+                            </button>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </form>
     </section>
     );
   }
--- src/components/Drivers.js
+++ src/components/Drivers.js
@@ -3,32 +3,30 @@ function Drivers(props) {
     let opts = {};
 
     opts['disabled'] = 'disabled';
-    
-    console.log(props.selectedDriver)
 
     return (
         <>
         <option>Select driver</option>
-        <option value="LH" id="LH" disabled={props.selectedDriver === 'LH' ? true : false}>44. Lewis Hamilton 🇬🇧</option>
-        <option value="MV" id="MV">33. Max Verstappen 🇳🇱</option>
-        <option value="VB" id="VB">77. Valterri Bottas 🇫🇮</option>
-        <option value="SP" id="SP">5. Sergio Perez 🇲🇽</option>
-        <option value="LN" id="LN">4. Lando Norris 🇬🇧</option>
-        <option value="DR" id="DR">3. Daniel Ricciardo 🇦🇺</option>
-        <option value="CS" id="CS">55. Carlos Sainz 🇪🇸</option>
-        <option value="CL" id="CL">16. Charles Leclerc 🇲🇨</option>
-        <option value="FA" id="FA">14. Fernando Alonso 🇪🇸</option>
-        <option value="EO" id="EO">31. Esteban Ocon 🇫🇷</option>
-        <option value="SV" id="SV">5. Sebastian Vettel 🇩🇪</option>
-        <option value="LS" id="LS">18. Lance Stroll 🇨🇦</option>
-        <option value="YS" id="YS">22. Yuki Tsunoda 🇯🇵</option>
-        <option value="PG" id="PG">10. Pierre Gasly 🇫🇷</option>
-        <option value="KR" id="KR">7. Kimi Raikkonen 🇫🇮</option>
-        <option value="AG" id="AG">99. Antonio Giovanazzi 🇮🇹</option>
-        <option value="GR" id="GR">63. George Russell 🇬🇧</option>
-        <option value="NL" id="NL">6. Nicolas Latifi 🇨🇦</option>
-        <option value="MS" id="MS">47. Mick Schumacher 🇩🇪</option>
-        <option value="NM" id="NM" disabled>9. Nikita Mazepin 🇷🇺</option>
+        <option value="LH" id="LH" disabled={props.podium.some(d => d['name'] === 'LH') ? true : false}>44. Lewis Hamilton 🇬🇧</option>
+        <option value="MV" id="MV" disabled={props.podium.some(d => d['name'] === 'MV')  ? true : false}>33. Max Verstappen 🇳🇱</option>
+        <option value="VB" id="VB" disabled={props.podium.some(d => d['name'] === 'VB')  ? true : false}>77. Valterri Bottas 🇫🇮</option>
+        <option value="SP" id="SP" disabled={props.podium.some(d => d['name'] === 'SP')  ? true : false}>5. Sergio Perez 🇲🇽</option>
+        <option value="LN" id="LN" disabled={props.podium.some(d => d['name'] === 'LN')  ? true : false}>4. Lando Norris 🇬🇧</option>
+        <option value="DR" id="DR" disabled={props.podium.some(d => d['name'] === 'DR')  ? true : false}>3. Daniel Ricciardo 🇦🇺</option>
+        <option value="CS" id="CS" disabled={props.podium.some(d => d['name'] === 'CS')  ? true : false}>55. Carlos Sainz 🇪🇸</option>
+        <option value="CL" id="CL" disabled={props.podium.some(d => d['name'] === 'CL')  ? true : false}>16. Charles Leclerc 🇲🇨</option>
+        <option value="FA" id="FA" disabled={props.podium.some(d => d['name'] === 'FA')  ? true : false}>14. Fernando Alonso 🇪🇸</option>
+        <option value="EO" id="EO" disabled={props.podium.some(d => d['name'] === 'EO')  ? true : false}>31. Esteban Ocon 🇫🇷</option>
+        <option value="SV" id="SV" disabled={props.podium.some(d => d['name'] === 'SV')  ? true : false}>5. Sebastian Vettel 🇩🇪</option>
+        <option value="LS" id="LS" disabled={props.podium.some(d => d['name'] === 'LS')  ? true : false}>18. Lance Stroll 🇨🇦</option>
+        <option value="YS" id="YS" disabled={props.podium.some(d => d['name'] === 'YT')  ? true : false}>22. Yuki Tsunoda 🇯🇵</option>
+        <option value="PG" id="PG" disabled={props.podium.some(d => d['name'] === 'PG')  ? true : false}>10. Pierre Gasly 🇫🇷</option>
+        <option value="KR" id="KR" disabled={props.podium.some(d => d['name'] === 'KR')  ? true : false}>7. Kimi Raikkonen 🇫🇮</option>
+        <option value="AG" id="AG" disabled={props.podium.some(d => d['name'] === 'AG')  ? true : false}>99. Antonio Giovanazzi 🇮🇹</option>
+        <option value="GR" id="GR" disabled={props.podium.some(d => d['name'] === 'GR')  ? true : false}>63. George Russell 🇬🇧</option>
+        <option value="NL" id="NL" disabled={props.podium.some(d => d['name'] === 'NT')  ? true : false}>6. Nicolas Latifi 🇨🇦</option>
+        <option value="MS" id="MS" disabled={props.podium.some(d => d['name'] === 'MS')  ? true : false}>47. Mick Schumacher 🇩🇪</option>
+        <option value="NM" id="NM" disabled={props.podium.some(d => d['name'] === 'NZ')  ? true : false}>9. Nikita Mazepin 🇷🇺</option>
         </>
     );
 }
